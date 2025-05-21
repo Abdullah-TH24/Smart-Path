f984642f32dad8500254d664b030dc101a4c9309
mohamad serafi <mohamad1serafi@gmail.com>
2025-05-21 23:53:24 +0300

    api class and assets added to code

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  static const String iconHomeInactive = "assets/icons/home_inactive.svg";
+
+  static const String iconAbout = "assets/icons/icon_about.svg";
+
+  static const String iconAchievment = "assets/icons/icon_achievment_ldpi.svg";
+
+  static const String iconArticle = "assets/icons/icon_article_ldpi.svg";
+
+  static const String iconBook = "assets/icons/icon_book.svg";
+
+  static const String iconCalendar = "assets/icons/icon_calendar_ldpi.svg";
+
+  static const String iconCase = "assets/icons/icon_case.svg";
+
+  static const String iconChangePassword =
+      "assets/icons/icon_change_password_ldpi.svg";
+
+  static const String iconClass = "assets/icons/icon_class_ldpi.svg";
+
+  static const String iconContact = "assets/icons/icon_contact_ldpi.svg";
+
+  static const String iconEditProfile =
+      "assets/icons/icon_edit_profile_ldpi.svg";
+
+  static const String iconFolder = "assets/icons/icon_folder.svg";
+
+  static const String iconFolderInactive =
+      "assets/icons/icon_folder_inactive.svg";
+
+  static const String iconHelp = "assets/icons/icon_help.svg";
+
+  static const String iconHomeActive = "assets/icons/icon_home_active.svg";
+
+  static const String iconInputInactive =
+      "assets/icons/icon_input_inactive.svg";
+
+  static const String iconLamp = "assets/icons/icon_lamp.svg";
+
+  static const String iconProfileActive =
+      "assets/icons/icon_profile_active_ldpi.svg";
+
+  static const String iconProfileInactive =
+      "assets/icons/icon_profile_inactive.svg";
+
+  static const String iconQuiz = "assets/icons/icon_quiz.svg";
+
+  static const String iconSignout = "assets/icons/icon_signout_ldpi.svg";
+
+  static const String iconStats = "assets/icons/icon_stats.svg";
+
+  static const String iconStudent = "assets/icons/icon_student.svg";
+
+  static const String iconTeacher = "assets/icons/icon_teacher.svg";
+
+  static const String iconInputEnabled = "assets/icons/input_enabled.svg";
+
+  static const String logo = "assets/icons/logo.svg";
+}
